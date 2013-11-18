1f876fba32d6f9c1eccee9e150cfe808fb991386
gretarg09 <gretarg09@ru.is>
2013-11-18 21:44:40 +0000

    starting to get the web server going. Change the main function, added a index and change the pom.xml

--- src/main/java/is/ru/tictactoe/TicTacToe.java
+++ src/main/java/is/ru/tictactoe/TicTacToe.java
@@ -1,173 +1,172 @@
 package is.ru.tictactoe;
 
 import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import static spark.Spark.*;
+import spark.*;
 
 public class TicTacToe {
-	
-	int x;
-	char grid[];
-	int player;
-	
+    int x;
+    char grid[];
+    int player;
     
-	public TicTacToe() {
-		grid = new char[9];
-		player = 1;
-		initializedGrid();
-	}
+    public TicTacToe() 
+    {
+        grid = new char[9];
+        player = 1;
+        initializedGrid();
+    }
 
+    public void initializedGrid()
+    {
+        for (int i = 0; i < 9; i++){
+            grid[i] = '-';
+        }
+    }
 
-	public void initializedGrid(){
-		for (int i = 0; i < 9; i++){
-			grid[i] = '-';
-		}
-	}
+    // Checks who is winner
+    public boolean checkForWin(){
+        if((winningRow() == true || winningColumn() == true) || winningDiagonal() == true){
+            return true;
+        }
+        return false;
+    }
 
-	// prints out the TicTacToe board
-	public void print(){
-		System.out.println();
-		System.out.println("Current status:");
-		for (int i = 0; i < 9; i++){
-			System.out.print(" " + grid[i] + " ");
-			if(i == 2 || i == 5 || i == 8){
-				System.out.println();
-			}
-		}
-	}
+    // Checks if there is a winner in a row
 
+    private boolean winningRow(){
+        
+        if(grid[0] != '-' && grid[0] == grid[1] && grid[1] == grid[2]){
+            return true;
+        }
+        if(grid[3] != '-' && grid[3] == grid[4] && grid[4] == grid[5]){
+            return true;
+        }
+        if(grid[6] != '-' && grid[6] == grid[7] && grid[7] == grid[8]){
+            return true;
+        }
+        return false;
+    }
 
-	// Checks who is winner
-	public boolean checkForWin(){
-		if((winningRow() == true || winningColumn() == true) || winningDiagonal() == true){
-			return true;
-		}
-		return false;
-	}
+    // Checks if there is a winner in a column
+    private boolean winningColumn(){
+        if(grid[0] != '-' && grid[0] == grid[3] && grid[3] == grid[6]){
+            return true;
+        }
+        if(grid[1] != '-' && grid[1] == grid[4] && grid[4] == grid[7]){
+            return true;
+        }
+        if(grid[2] != '-' &&grid[2] == grid[5] && grid[5] == grid[8]){
+            return true;
+        }
+        return false;
+    }
+    // Checks if there is a winner diagonally
+    private boolean winningDiagonal(){
+        if(grid[0] != '-' && grid[0] == grid[4] && grid[4] == grid[8]){
+            return true;
+        }       
+        if(grid[2] != '-' && grid[2] == grid[4] && grid[4] == grid[6]){
+            return true;
+        }
+        return false;
+    }
 
-	// Checks if there is a winner in a row
+    // function to check if the input is valid
+    public boolean checkIfValidInput(){
+        // check if the grid is whithin the limit of the array
+        if ( x > 8 || x < 0){
+            return false;
+        }
+        // check if the grid is empty
+        if ( grid[x] != '-'){
+            return false;
+        }
+        return true;
+    }
 
-	private boolean winningRow(){
-		
-		if(grid[0] != '-' && grid[0] == grid[1] && grid[1] == grid[2]){
-			return true;
-		}
-		if(grid[3] != '-' && grid[3] == grid[4] && grid[4] == grid[5]){
-			return true;
-		}
-		if(grid[6] != '-' && grid[6] == grid[7] && grid[7] == grid[8]){
-			return true;
-		}
-		return false;
-	}
-
-        // Checks if there is a winner in a column
-
-	private boolean winningColumn(){
-		if(grid[0] != '-' && grid[0] == grid[3] && grid[3] == grid[6]){
-			return true;
-		}
-		if(grid[1] != '-' && grid[1] == grid[4] && grid[4] == grid[7]){
-			return true;
-		}
-		if(grid[2] != '-' &&grid[2] == grid[5] && grid[5] == grid[8]){
-			return true;
-		}
-		return false;
-	}
-        // Checks if there is a winner diagonally
-	private boolean winningDiagonal(){
-		if(grid[0] != '-' && grid[0] == grid[4] && grid[4] == grid[8]){
-			return true;
-		}		
-		if(grid[2] != '-' && grid[2] == grid[4] && grid[4] == grid[6]){
-			return true;
-		}
-		return false;
-	}
-
-	// function to check if the input is valid
-	public boolean checkIfValidInput(){
-		// check if the grid is whithin the limit of the array
-		if ( x > 8 || x < 0){
-			return false;
-		}
-		// check if the grid is empty
-		if ( grid[x] != '-'){
-			return false;
-		}
-		return true;
-	}
-
-	//check for tie
-	public boolean checkForTie() {
-		for (int i = 0; i < 9; i++){
-			if (grid[i] == '-') return false;
-		}
-		return true;
-	}
+    //check for tie
+    public boolean checkForTie() {
+        for (int i = 0; i < 9; i++){
+            if (grid[i] == '-') return false;
+        }
+        return true;
+    }
      
-	public void changePlayer(){
-		if (player == 1) player = 2;
-		else player = 1;
-	}
+    public void changePlayer(){
+        if (player == 1) player = 2;
+        else player = 1;
+    }
 
-	public void insert(){
-		if(player == 1){
-			grid[x] = 'X';	
-		}
-		else{
-			grid[x] = 'O';
-		}
-	}
-
+    public void insert(){
+        if(player == 1){
+            grid[x] = 'X';  
+        }
+        else{
+            grid[x] = 'O';
+        }
+    }
    
-	public void play(){
-		Scanner in = new Scanner(System.in);
-		boolean validInput;
-		
-		while(!checkForWin() && !checkForTie()){
-			validInput = false;
-			print();
-			System.out.println("Player " + player + " it's your turn");
+    public void play(){
+        Scanner in = new Scanner(System.in);
+        boolean validInput;
+        
+        while(!checkForWin() && !checkForTie()){
+            validInput = false;
 
-			do{
-			System.out.println("Add your next move: ");
-			System.out.print("What grid do you want (0-8): ");
-			x = in.nextInt();
-			if(checkIfValidInput()){
-				insert();
-				validInput = true;
-			};
-			}while (!validInput);	
-			changePlayer();			
-		}
+            do{
+           
+            x = in.nextInt();
+            if(checkIfValidInput()){
+                insert();
+                validInput = true;
+            };
+            }while (!validInput);   
+            changePlayer();         
+        }
 
-		if(checkForWin()){
-			print();
-			changePlayer();
-			System.out.println();
-			System.out.println("Player " + player + " WON!!!!! ");
-		}
-		else if(checkForTie()){
-			print();
-			System.out.println("We have a TIE!!!!!! ");
-		}
+        if(checkForWin())
+        {
+            changePlayer();
+            System.out.println();
+            System.out.println("Player " + player + " WON!!!!! ");
+        }
+        else if(checkForTie())
+        {
+            System.out.println("We have a TIE!!!!!! ");
+        }
+        in.close();
+    }
+    
 
-		in.close();
-	}
-	
-	
+    public static void main(String[] args) 
+    {
+        staticFileLocation("/public");
+        setPort(Integer.valueOf(System.getenv("PORT")));
+        final TicTacToe tictactoe = new TicTacToe();
 
-	public static void main(String[] args) {
-		TicTacToe mainTest = new TicTacToe();
-		System.out.println("Hello and welcome to a game of Tic Tac Toe");
-		System.out.println();
-		System.out.println("The playground is described below");
-		System.out.println("_________");
-		System.out.println("| 0 1 2 |");
-		System.out.println("| 3 4 5 |");
-		System.out.println("| 6 7 8 |");
-		mainTest.play();
-	}
+        post(new Route("/add") 
+        {
+            @Override
+            public Object handle(Request request, Response response) 
+            {
+                System.out.println("Pálmi er awesome");
+                Integer gridId = Integer.valueOf(request.queryParams("gridId"));
+                tictactoe.x = gridId;
+                if(!tictactoe.checkIfValidInput()){
+                    return 0;
+                }
+                else{
+                    tictactoe.insert();
+                    tictactoe.changePlayer();
+                    return tictactoe.player; 
+                }
+            }
+        });
+
+
+    }
 }
-
-
