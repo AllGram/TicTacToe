1f84fb8a96825697119d433c6ba17fa70cc89021
Kris <kristjanj11@ru.is>
2013-11-23 14:03:46 +0000

    Selenium test fixed

--- src/test/java/is/ru/tictactoe/Selenium1.java
+++ src/test/java/is/ru/tictactoe/Selenium1.java
@@ -1,15 +1,39 @@
-import junit.framework.Test;
-import junit.framework.TestSuite;
+package com.example.tests;
+
+import com.thoughtworks.selenium.Selenium;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverBackedSelenium;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.util.regex.Pattern;
+import static org.apache.commons.lang3.StringUtils.join;
 
 public class Selenium1 {
+	private Selenium selenium;
 
-	public static Test suite() {
-		TestSuite suite = new TestSuite();
-		suite.addTestSuite(Untitled.class);
-		return suite;
+	@Before
+	public void setUp() throws Exception {
+		WebDriver driver = new FirefoxDriver();
+		String baseUrl = "http://allgram.herokuapp.com";
+		selenium = new WebDriverBackedSelenium(driver, baseUrl);
 	}
 
-	public static void main(String[] args) {
-		junit.textui.TestRunner.run(suite());
+	@Test
+	public void testSelenium1() throws Exception {
+		selenium.open("/");
+		selenium.click("id=0");
+		selenium.click("id=4");
+		selenium.click("id=1");
+		selenium.click("id=5");
+		selenium.click("id=2");
+		assertEquals("Player one WINS!!!!", selenium.getAlert());
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		selenium.stop();
 	}
 }
